e83a46fb28dd385b01d9455496aa8324c7b96ee8
Evgen <Kadett0303@gmail.com>
2021-03-16 00:49:17 +0300

    Completed task 1

--- lesson1.2.cpp
+++ lesson1.2.cpp
@@ -1,9 +1,27 @@
-﻿
+﻿/*
+/   lesson1.2.cpp
+/
+/   Onishchenko Evgeniy
+/
+/
+*/
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Task 1
+    {
+        short a = 0;
+        int b = 0;
+        long long c = 0;
+        char d = '0';
+        bool b= true;
+        float e = 0.0f;
+        double f = 0.0;
+    }
+
+
+
 }
 
